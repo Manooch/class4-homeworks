4bed166382df8b2b0bc5c6ede32f53cf18ee0984
Manooch <kian.manoochehri@gmail.com>
2019-02-10 19:41:19 -0500

    Casted and Transposed for Boston DataSet

---
+++ cct_parser_Boston.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+
+myfilename = "housing.data.txt"
+
+
+with open(myfilename, 'r') as file_handle:
+    mylist = []
+    for line in file_handle.readlines():
+        line_clean = line.replace('   ', ' ').replace('  ', ' ')
+        line_clean = line_clean.strip()
+        values = line_clean.split(' ')
+        linelist = []
+        for value in values:
+            try:
+                linelist += [int(value)]
+            except ValueError as e:
+                linelist += [float(value)]
+        mylist += [linelist]
+
+print("just printing a bit to save our eyes....")
+print(mylist[0:2])
+rotated_list = [[mylist[jdx][idx]
+                 for jdx, row in enumerate(mylist)]
+                for idx, column in enumerate(mylist[0])]
+print(rotated_list[0])
+
